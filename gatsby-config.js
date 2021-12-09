1890293fcd01e8f1d17b7c80abcc1daf67611cc9
jamaspy <aspin@me.com>
2021-12-09 16:20:10 +1100

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,18 @@ module.exports = {
         icon: "src/images/icon.png",
       },
     },
-    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: [`auto`, `webp`],
+          placeholder: `dominantColor`,
+          quality: 90,
+          breakpoints: [750, 1080, 1366, 1920],
+          backgroundColor: `transparent`,
+        },
+      },
+    },
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,9 @@ module.exports = {
       },
       colors: {
         primary: "#FF6363",
+        "primary-hover": "#EC1C1C",
         secondary: "#4100FB",
+        "secondary-hover": "#3200BF",
         tertiary: "#FFD15C",
         blacked: "#070717",
         "gray-dark": "#696466",
@@ -17,6 +19,7 @@ module.exports = {
         "gray-lighter": "#F7F7F9",
         success: "#00845C",
         error: "#C70030",
+        "hover-blue": "#3725F1",
       },
     },
   },
