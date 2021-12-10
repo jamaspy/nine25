34e92357767c174d63aafff36016cfd63c6093a8
jamaspy <aspin@me.com>
2021-12-10 19:30:51 +1100

    check for device function

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -4,7 +4,7 @@ import { radialFill } from "../../styles/textGradientStyleObjects";
 import { Download_Android, Download_IOS } from "../Buttons";
 import HeroSVG from "../../assets/hero.svg";
 import StarSVG from "../../assets/yellow_star.svg";
-const index = () => {
+const index = ({ device }) => {
   return (
     <div className="flex flex-col flex-shrink mt-12 md:flex-row md:justify-center relative overflow-hidden">
       <div className="md:pl-4">
@@ -21,8 +21,14 @@ const index = () => {
             manage & invest your salary.
           </p>
           <div className="flex flex-row w-full justify-center md:justify-start mt-12">
-            <Download_IOS className="m-0 md:mr-8" />
-            <Download_Android className="hidden lg:inline-block" />
+            {device() === "ios" && <Download_IOS />}
+            {device() === "android" && <Download_Android />}
+            {device() === "desktop" && (
+              <>
+                <Download_IOS className="mr-8" />
+                <Download_Android />
+              </>
+            )}
           </div>
         </div>
         <div className="absolute -bottom-10 left-0 hidden md:block">
