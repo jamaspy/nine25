2a4f20d44ec73cdde483f91b591858c148e8202b
jamaspy <aspin@me.com>
2021-12-10 15:31:10 +1100

    working in iPad layout

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -6,7 +6,7 @@ import HeroSVG from "../../assets/hero.svg";
 import StarSVG from "../../assets/yellow_star.svg";
 const index = () => {
   return (
-    <div className="flex flex-col mt-12 md:flex-row md:justify-center relative overflow-hidden">
+    <div className="flex flex-col flex-shrink mt-12 md:flex-row md:justify-center relative overflow-hidden">
       <div className="">
         <div className="flex flex-col items-center justify-center xl:justify-start lg:items-start lg:mt-24 lg:ml-24">
           <p className="text-4xl md:text-5xl font-black">Now Streaming:</p>
@@ -29,7 +29,7 @@ const index = () => {
           <StarSVG />
         </div>
       </div>
-      <div className="flex items-center -mt-36 md:mt-0 justify-end lg:mr-24">
+      <div className="flex flex-shrink items-center -mt-36 md:mt-0 justify-end lg:mr-24">
         <HeroSVG />
       </div>
     </div>
