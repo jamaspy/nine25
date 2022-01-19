644f9fb0369fd26817d502e74241762477562fd7
jamaspy <aspin@me.com>
2022-01-19 21:05:56 +1100

    change size

--- src/components/Buttons/DownloadIOS.jsx
+++ src/components/Buttons/DownloadIOS.jsx
@@ -10,7 +10,7 @@ const DownloadIOS = ({ className, location }) => {
             `https://apps.apple.com/app/apple-store/id1540723857?pt=122259206&ct=website-${location}&mt=8`
           );
       }}
-      style={{ minWidth: "286px", minHeight: "85px" }}
+      style={{ minWidth: "250px", minHeight: "65px" }}
       className={`bg-blacked text-white border rounded-full  flex items-center justify-center hover:cursor-pointer ${className} mx-auto`}
     >
       <ITunesSVG />
