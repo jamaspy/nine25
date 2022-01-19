7856e00ab0a2451512d936b827d930aa59309ac3
jamaspy <aspin@me.com>
2022-01-19 21:28:31 +1100

    stop expansion at lage size

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -9,54 +9,59 @@ import DownloadModal from "../DownloadModal";
 import Typical from "./Typing";
 const index = ({ device }) => {
   return (
-    <div className="flex flex-col flex-shrink mt-12 md:flex-row md:justify-center relative overflow-hidden">
+    <div className="flex flex-col flex-grow-0 flex-shrink mt-12 md:flex-row md:justify-center relative overflow-hidden">
       <DownloadModal />
-      <div className="">
-        <div className="flex flex-col items-center justify-center xl:justify-start xl:items-start 2xl:mt-24 2xl:ml-24">
-          <p className="text-4xl md:text-5xl font-black">Now Streaming:</p>
-          <p className="text-4xl md:text-5xl font-bold my-2" style={radialFill}>
-            Your salary.
-          </p>
-          <div className="flex flex-row flex-wrap items-center justify-center mt-4 text-xl text-center md:text-left px-4 md:p-0">
-            <p>The</p>{" "}
-            <span className="bg-primary mx-1">
-              <Typical
-                steps={[
-                  "revolutionary",
-                  2000,
-                  "dependable",
-                  2000,
-                  "adaptable",
-                  2000,
-                ]}
-                loop={Infinity}
-                wrapper="p"
-              />
-            </span>
-            <p className="">way to spend, manage & invest your salary.</p>
-          </div>
-          <div className="flex flex-row w-full justify-center md:justify-start mt-12">
-            {device() === "ios" && <DownloadIOS location="body" />}
-            {device() === "android" && <DownloadAndroid />}
-            {device() === "desktop" && (
-              <>
-                <DownloadIOS className="mr-8" />
-                <DownloadAndroid />
-              </>
-            )}
-          </div>
+
+      <div className="px-2 md:min-w-[43%]">
+        <p className="text-4xl md:text-5xl font-black text-center md:text-left">
+          Now Streaming:
+        </p>
+        <p
+          className="text-4xl md:text-5xl font-bold my-2 text-center md:text-left"
+          style={radialFill}
+        >
+          Your salary.
+        </p>
+        <div className="flex flex-row flex-wrap items-center justify-center md:items-start md:justify-start mt-4 text-xl text-center md:text-left px-4 md:p-0 ">
+          <p>The</p>{" "}
+          <span className="bg-primary mx-1">
+            <Typical
+              steps={[
+                "revolutionary",
+                2000,
+                "dependable",
+                2000,
+                "adaptable",
+                2000,
+              ]}
+              loop={Infinity}
+              wrapper="p"
+            />
+          </span>
+          <p className="">way to spend, manage & invest your salary.</p>
         </div>
-        <div className="md:hidden absolute lg:-bottom-20 left-0 hidden lg:block">
-          <StarSVG />
+        <div className="flex flex-row grow-0 w-full justify-start mt-12">
+          {device() === "ios" && <DownloadIOS location="body" />}
+          {device() === "android" && <DownloadAndroid />}
+          {device() === "desktop" && (
+            <>
+              <DownloadIOS className="mr-8" />
+              <DownloadAndroid />
+            </>
+          )}
         </div>
       </div>
-      <div className="flex lg:hidden flex-shrink items-center">
+      <div className=" absolute lg:-bottom-20 left-0 hidden lg:block">
+        <StarSVG />
+      </div>
+
+      <div className="flex md:hidden flex-shrink items-center">
         <HeroSVG />
       </div>
-      <div className="hidden md:flex xl:hidden flex-shrink items-center">
+      <div className="hidden md:flex xl:hidden flex-shrink items-end mt-36">
         <HeroTabletSVG />
       </div>
-      <div className="hidden xl:flex flex-shrink items-center">
+      <div className="hidden xl:flex flex-shrink items-end justify-end">
         <HeroMobileSVG />
       </div>
     </div>
